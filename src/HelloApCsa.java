d9753e0110af505b614638c458d785a861ca6958
BT_1N3_32 <sample@bths.edu>
2024-10-25 10:09:22 -0400

    changed runner and added a new class "HelloApCsa"

---
+++ src/HelloApCsa.java
@@ -0,0 +1,6 @@
+public class HelloApCsa {
+    public static void main(String[] args) {
+        System.out.println("hello from ap csa");
+        System.out.println("how ya doin");
+    }
+}
--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,9 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello AP CSA!");
-        System.out.println("I like oranges and bananas");
+        System.out.println("I like oranges and apples");
         int nine = 4+5;
+        System.out.println("I love addition!");
         System.out.println(nine);
         int something = (int) (Math.random()*5+1);
         System.out.println("The lucky number is: " + something);
