54c02b7bddedab946ff14d695f3198d6437a8c1d
BT_1N3_32 <sample@bths.edu>
2024-10-25 10:05:26 -0400

    added a random variable and a print, also made some changes to existing code.

--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,10 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello AP CSA!");
-        System.out.println("I like apples and bananas");
+        System.out.println("I like oranges and bananas");
         int nine = 4+5;
         System.out.println(nine);
+        int something = (int) (Math.random()*5+1);
+        System.out.println("The lucky number is: " + something);
     }
 }
