ec3f159e844907d3c212e49a943ab645834f7e70
BT_1N3_32 <sample@bths.edu>
2024-10-25 10:03:17 -0400

    added a few print statements and a variable

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,8 @@
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello AP CSA!");
+        System.out.println("I like apples and bananas");
+        int nine = 4+5;
+        System.out.println(nine);
     }
 }
